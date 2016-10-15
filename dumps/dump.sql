fe25d95e7b0c2abafae82291454e6411707eeaa3
Guillaume Denis <denisgme@gmail.com>
2016-10-15 15:55:05 +0200

    add: new db dump

--- dumps/dump.sql
+++ dumps/dump.sql
@@ -45,7 +45,7 @@ CREATE TABLE `DATABASECHANGELOG` (
 
 LOCK TABLES `DATABASECHANGELOG` WRITE;
 /*!40000 ALTER TABLE `DATABASECHANGELOG` DISABLE KEYS */;
-INSERT INTO `DATABASECHANGELOG` VALUES ('00000000000001','jhipster','classpath:config/liquibase/changelog/00000000000000_initial_schema.xml','2016-10-10 19:06:20',1,'EXECUTED','7:05d6c1de129cdd107606d5dba8731152','createTable, createIndex (x2), createTable (x2), addPrimaryKey, createTable, addForeignKeyConstraint (x3), loadData, dropDefaultValue, loadData (x2), createTable (x2), addPrimaryKey, createIndex (x2), addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161010150030-1','jhipster','classpath:config/liquibase/changelog/20161010150030_added_entity_Emprunt.xml','2016-10-10 19:06:20',2,'EXECUTED','7:16b3988bf5a6bddb52109eaaa98c487d','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150031-1','jhipster','classpath:config/liquibase/changelog/20161010150031_added_entity_Usager.xml','2016-10-10 19:06:20',3,'EXECUTED','7:871dbcd81e2b1867d10975a9733433d0','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150032-1','jhipster','classpath:config/liquibase/changelog/20161010150032_added_entity_Livre.xml','2016-10-10 19:06:20',4,'EXECUTED','7:1e9922220fd626672ccc5ed2dc042cf7','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150033-1','jhipster','classpath:config/liquibase/changelog/20161010150033_added_entity_Exemplaire.xml','2016-10-10 19:06:20',5,'EXECUTED','7:b3c638855a5000207e42d86e7aec0a91','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150034-1','jhipster','classpath:config/liquibase/changelog/20161010150034_added_entity_Auteur.xml','2016-10-10 19:06:20',6,'EXECUTED','7:a47d49ad2e1cbfad25b554a67b1fc115','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150035-1','jhipster','classpath:config/liquibase/changelog/20161010150035_added_entity_Magazine.xml','2016-10-10 19:06:20',7,'EXECUTED','7:d460edee3bbeb70432f440dc420a5b26','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150036-1','jhipster','classpath:config/liquibase/changelog/20161010150036_added_entity_Reservation.xml','2016-10-10 19:06:20',8,'EXECUTED','7:b43762dc2a1c5bae609501f4759c6282','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150030-2','jhipster','classpath:config/liquibase/changelog/20161010150030_added_entity_constraints_Emprunt.xml','2016-10-10 19:06:20',9,'EXECUTED','7:9a3b7e421d47c2b1b4b5a41ff96053c1','addForeignKeyConstraint (x3)','',NULL,'3.4.2',NULL,NULL),('20161010150032-2','jhipster','classpath:config/liquibase/changelog/20161010150032_added_entity_constraints_Livre.xml','2016-10-10 19:06:20',10,'EXECUTED','7:8c75cfe53df870acc17e5223d7264541','addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161010150033-2','jhipster','classpath:config/liquibase/changelog/20161010150033_added_entity_constraints_Exemplaire.xml','2016-10-10 19:06:21',11,'EXECUTED','7:56be8da1955d207dead0e35f6693ee9b','addForeignKeyConstraint (x3)','',NULL,'3.4.2',NULL,NULL),('20161010150035-2','jhipster','classpath:config/liquibase/changelog/20161010150035_added_entity_constraints_Magazine.xml','2016-10-10 19:06:21',12,'EXECUTED','7:c391ef493b48040cfa32dc8a73c4974d','addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161010150036-2','jhipster','classpath:config/liquibase/changelog/20161010150036_added_entity_constraints_Reservation.xml','2016-10-10 19:06:21',13,'EXECUTED','7:22a6f2cfa7a26302a0e5b0687009d886','addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL);
+INSERT INTO `DATABASECHANGELOG` VALUES ('00000000000001','jhipster','classpath:config/liquibase/changelog/00000000000000_initial_schema.xml','2016-10-15 08:36:35',1,'EXECUTED','7:05d6c1de129cdd107606d5dba8731152','createTable, createIndex (x2), createTable (x2), addPrimaryKey, createTable, addForeignKeyConstraint (x3), loadData, dropDefaultValue, loadData (x2), createTable (x2), addPrimaryKey, createIndex (x2), addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161015074820-1','jhipster','classpath:config/liquibase/changelog/20161015074820_added_entity_Usager.xml','2016-10-15 08:44:27',2,'EXECUTED','7:871dbcd81e2b1867d10975a9733433d0','createTable','',NULL,'3.4.2',NULL,NULL),('20161015074823-1','jhipster','classpath:config/liquibase/changelog/20161015074823_added_entity_Auteur.xml','2016-10-15 08:44:27',3,'EXECUTED','7:a47d49ad2e1cbfad25b554a67b1fc115','createTable','',NULL,'3.4.2',NULL,NULL),('20161015074821-1','jhipster','classpath:config/liquibase/changelog/20161015074821_added_entity_Livre.xml','2016-10-15 08:44:27',4,'EXECUTED','7:1e9922220fd626672ccc5ed2dc042cf7','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150035-1','jhipster','classpath:config/liquibase/changelog/20161010150035_added_entity_Magazine.xml','2016-10-15 08:44:27',5,'EXECUTED','7:d460edee3bbeb70432f440dc420a5b26','createTable','',NULL,'3.4.2',NULL,NULL),('20161015074822-1','jhipster','classpath:config/liquibase/changelog/20161015074822_added_entity_Exemplaire.xml','2016-10-15 08:44:27',6,'EXECUTED','7:2eee0abf2a3af0eb497afdbb0599d317','createTable','',NULL,'3.4.2',NULL,NULL),('20161010150030-1','jhipster','classpath:config/liquibase/changelog/20161010150030_added_entity_Emprunt.xml','2016-10-15 08:44:27',7,'EXECUTED','7:16b3988bf5a6bddb52109eaaa98c487d','createTable','',NULL,'3.4.2',NULL,NULL),('20161015074825-1','jhipster','classpath:config/liquibase/changelog/20161015074825_added_entity_Reservation.xml','2016-10-15 08:44:27',8,'EXECUTED','7:6f2fc634ff14f54e85121b83595be423','createTable','',NULL,'3.4.2',NULL,NULL),('20161015074821-2','jhipster','classpath:config/liquibase/changelog/20161015074821_added_entity_constraints_Livre.xml','2016-10-15 08:44:27',9,'EXECUTED','7:8c75cfe53df870acc17e5223d7264541','addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161010150035-2','jhipster','classpath:config/liquibase/changelog/20161010150035_added_entity_constraints_Magazine.xml','2016-10-15 08:44:27',10,'EXECUTED','7:c391ef493b48040cfa32dc8a73c4974d','addForeignKeyConstraint','',NULL,'3.4.2',NULL,NULL),('20161015074822-2','jhipster','classpath:config/liquibase/changelog/20161015074822_added_entity_constraints_Exemplaire.xml','2016-10-15 08:44:27',11,'EXECUTED','7:19562cad016701adbe6e9c7c694b3967','addForeignKeyConstraint (x2)','',NULL,'3.4.2',NULL,NULL),('20161010150030-2','jhipster','classpath:config/liquibase/changelog/20161010150030_added_entity_constraints_Emprunt.xml','2016-10-15 08:44:27',12,'EXECUTED','7:9a3b7e421d47c2b1b4b5a41ff96053c1','addForeignKeyConstraint (x3)','',NULL,'3.4.2',NULL,NULL),('20161015074825-2','jhipster','classpath:config/liquibase/changelog/20161015074825_added_entity_constraints_Reservation.xml','2016-10-15 08:44:27',13,'EXECUTED','7:7c0516f95b492f552b13b6b36bbe37c5','addForeignKeyConstraint (x2)','',NULL,'3.4.2',NULL,NULL);
 /*!40000 ALTER TABLE `DATABASECHANGELOG` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +87,7 @@ CREATE TABLE `auteur` (
   `nom` varchar(255) DEFAULT NULL,
   `prenom` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +96,7 @@ CREATE TABLE `auteur` (
 
 LOCK TABLES `auteur` WRITE;
 /*!40000 ALTER TABLE `auteur` DISABLE KEYS */;
-INSERT INTO `auteur` VALUES (1,'Doe','John');
+INSERT INTO `auteur` VALUES (1,'DOE','John'),(2,'SEAGAL','Steven');
 /*!40000 ALTER TABLE `auteur` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +121,7 @@ CREATE TABLE `emprunt` (
   CONSTRAINT `fk_emprunt_livre_id` FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id`),
   CONSTRAINT `fk_emprunt_magazine_id` FOREIGN KEY (`magazine_id`) REFERENCES `magazine` (`id`),
   CONSTRAINT `fk_emprunt_usager_id` FOREIGN KEY (`usager_id`) REFERENCES `usager` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +130,6 @@ CREATE TABLE `emprunt` (
 
 LOCK TABLES `emprunt` WRITE;
 /*!40000 ALTER TABLE `emprunt` DISABLE KEYS */;
-INSERT INTO `emprunt` VALUES (1,'2016-10-14','2016-10-21',1,1,NULL);
 /*!40000 ALTER TABLE `emprunt` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,14 +146,11 @@ CREATE TABLE `exemplaire` (
   `etat` varchar(255) DEFAULT NULL,
   `livre_id` bigint(20) DEFAULT NULL,
   `magazine_id` bigint(20) DEFAULT NULL,
-  `reservation_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_exemplaire_livre_id` (`livre_id`),
   KEY `fk_exemplaire_magazine_id` (`magazine_id`),
-  KEY `fk_exemplaire_reservation_id` (`reservation_id`),
   CONSTRAINT `fk_exemplaire_livre_id` FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id`),
-  CONSTRAINT `fk_exemplaire_magazine_id` FOREIGN KEY (`magazine_id`) REFERENCES `magazine` (`id`),
-  CONSTRAINT `fk_exemplaire_reservation_id` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`)
+  CONSTRAINT `fk_exemplaire_magazine_id` FOREIGN KEY (`magazine_id`) REFERENCES `magazine` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -164,7 +160,7 @@ CREATE TABLE `exemplaire` (
 
 LOCK TABLES `exemplaire` WRITE;
 /*!40000 ALTER TABLE `exemplaire` DISABLE KEYS */;
-INSERT INTO `exemplaire` VALUES (1,'M2SID','Neuf',1,NULL,NULL);
+INSERT INTO `exemplaire` VALUES (1,'M2SID','Neuf',1,NULL);
 /*!40000 ALTER TABLE `exemplaire` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -205,7 +201,7 @@ CREATE TABLE `jhi_persistent_audit_event` (
   `event_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`event_id`),
   KEY `idx_persistent_audit_event` (`principal`,`event_date`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -214,7 +210,7 @@ CREATE TABLE `jhi_persistent_audit_event` (
 
 LOCK TABLES `jhi_persistent_audit_event` WRITE;
 /*!40000 ALTER TABLE `jhi_persistent_audit_event` DISABLE KEYS */;
-INSERT INTO `jhi_persistent_audit_event` VALUES (1,'admin','2016-10-10 21:09:10','AUTHENTICATION_SUCCESS'),(2,'admin','2016-10-13 13:43:12','AUTHENTICATION_SUCCESS');
+INSERT INTO `jhi_persistent_audit_event` VALUES (1,'admin','2016-10-15 10:37:35','AUTHENTICATION_SUCCESS'),(2,'admin','2016-10-15 11:33:32','AUTHENTICATION_SUCCESS'),(3,'user','2016-10-15 11:38:24','AUTHENTICATION_SUCCESS'),(4,'admin','2016-10-15 11:41:48','AUTHENTICATION_SUCCESS'),(5,'admin','2016-10-15 11:51:17','AUTHENTICATION_SUCCESS'),(6,'yolo','2016-10-15 11:51:41','AUTHENTICATION_FAILURE');
 /*!40000 ALTER TABLE `jhi_persistent_audit_event` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -241,7 +237,7 @@ CREATE TABLE `jhi_persistent_audit_evt_data` (
 
 LOCK TABLES `jhi_persistent_audit_evt_data` WRITE;
 /*!40000 ALTER TABLE `jhi_persistent_audit_evt_data` DISABLE KEYS */;
-INSERT INTO `jhi_persistent_audit_evt_data` VALUES (1,'remoteAddress','0:0:0:0:0:0:0:1'),(1,'sessionId','wt_esmolv5QWXbMqioYsBIqR5pWg8NrDy6JVUqEi'),(2,'remoteAddress','127.0.0.1'),(2,'sessionId','st0n7BWCXyJIB7Gou6LkGCdyvUFDjQLkyJzToJs9');
+INSERT INTO `jhi_persistent_audit_evt_data` VALUES (1,'remoteAddress','127.0.0.1'),(1,'sessionId','pEE17bLUA-U9MH1CO2Mpm7VUy2L8o-XWsjUwG_W8'),(2,'remoteAddress','127.0.0.1'),(2,'sessionId','VM4fpiJM5_PWbYC_YjTSNFFQ6GMrCPsRGOno6ptK'),(3,'remoteAddress','127.0.0.1'),(3,'sessionId','2S8BkUL7T19rl7w58xo_YT9MkgUATwXijWlLp6Hg'),(4,'remoteAddress','127.0.0.1'),(4,'sessionId','hH8FkngxuGcHkJuYBwfDdCf1g-faxuqY9xk274Y-'),(5,'remoteAddress','127.0.0.1'),(5,'sessionId','TDoO4dcI8CP7wO1DcuvEiopBD0hKxvbiXzXqV-2g'),(6,'message','Bad credentials'),(6,'remoteAddress','127.0.0.1'),(6,'sessionId','aRx0Uc0ZTw-RRc_BTrjv_TDUnQBxT_zt8nBivh9p'),(6,'type','org.springframework.security.authentication.BadCredentialsException');
 /*!40000 ALTER TABLE `jhi_persistent_audit_evt_data` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -271,7 +267,6 @@ CREATE TABLE `jhi_persistent_token` (
 
 LOCK TABLES `jhi_persistent_token` WRITE;
 /*!40000 ALTER TABLE `jhi_persistent_token` DISABLE KEYS */;
-INSERT INTO `jhi_persistent_token` VALUES ('oSPrIaOqclEv3Z9rt6TF+w==',3,'QpP8l30cj8nxqhHd8l64Iw==','2016-10-13','127.0.0.1','Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'),('YRSzWdfbnHU0/bZaRRL37g==',3,'oJqiIFZ2/hJDfadKJN1+/w==','2016-10-14','127.0.0.1','Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0');
 /*!40000 ALTER TABLE `jhi_persistent_token` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -303,7 +298,7 @@ CREATE TABLE `jhi_user` (
   UNIQUE KEY `idx_user_login` (`login`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `idx_user_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -312,7 +307,7 @@ CREATE TABLE `jhi_user` (
 
 LOCK TABLES `jhi_user` WRITE;
 /*!40000 ALTER TABLE `jhi_user` DISABLE KEYS */;
-INSERT INTO `jhi_user` VALUES (1,'system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System','System','system@localhost','','en',NULL,NULL,'system','2016-10-10 19:06:20',NULL,'system','2016-10-10 19:06:20'),(2,'anonymoususer','$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO','Anonymous','User','anonymous@localhost','','en',NULL,NULL,'system','2016-10-10 19:06:20',NULL,'system','2016-10-10 19:06:20'),(3,'admin','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','Administrator','Administrator','admin@localhost','','en',NULL,NULL,'system','2016-10-10 19:06:20',NULL,'system','2016-10-10 19:06:20'),(4,'user','$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K','User','User','user@localhost','','en',NULL,NULL,'system','2016-10-10 19:06:20',NULL,'system','2016-10-10 19:06:20');
+INSERT INTO `jhi_user` VALUES (1,'system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System','System','system@localhost','','en',NULL,NULL,'system','2016-10-15 08:36:35',NULL,'system','2016-10-15 08:36:35'),(2,'anonymoususer','$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO','Anonymous','User','anonymous@localhost','','en',NULL,NULL,'system','2016-10-15 08:36:35',NULL,'system','2016-10-15 08:36:35'),(3,'admin','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','Administrator','Administrator','admin@localhost','','en',NULL,NULL,'system','2016-10-15 08:36:35',NULL,'system','2016-10-15 08:36:35'),(4,'user','$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K','User','User','user@localhost','','en',NULL,NULL,'system','2016-10-15 08:36:35',NULL,'system','2016-10-15 08:36:35'),(5,'test','$2a$10$XJYpLT/oSh3LrMxuNF8B1.ayvi7CcgVFh9A84FKTElsY.u3MHm62O',NULL,NULL,'test@test.fr','\0','en','07930178155581055210',NULL,'anonymousUser','2016-10-15 11:51:36',NULL,'anonymousUser','2016-10-15 11:51:36');
 /*!40000 ALTER TABLE `jhi_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -339,7 +334,7 @@ CREATE TABLE `jhi_user_authority` (
 
 LOCK TABLES `jhi_user_authority` WRITE;
 /*!40000 ALTER TABLE `jhi_user_authority` DISABLE KEYS */;
-INSERT INTO `jhi_user_authority` VALUES (1,'ROLE_ADMIN'),(1,'ROLE_USER'),(3,'ROLE_ADMIN'),(3,'ROLE_USER'),(4,'ROLE_USER');
+INSERT INTO `jhi_user_authority` VALUES (1,'ROLE_ADMIN'),(1,'ROLE_USER'),(3,'ROLE_ADMIN'),(3,'ROLE_USER'),(4,'ROLE_USER'),(5,'ROLE_USER');
 /*!40000 ALTER TABLE `jhi_user_authority` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -369,7 +364,7 @@ CREATE TABLE `livre` (
 
 LOCK TABLES `livre` WRITE;
 /*!40000 ALTER TABLE `livre` DISABLE KEYS */;
-INSERT INTO `livre` VALUES (1,'2003-01-08','test',NULL,'2016-10-13',1);
+INSERT INTO `livre` VALUES (1,'1993-10-11','test',0,'2016-10-15',1);
 /*!40000 ALTER TABLE `livre` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -400,7 +395,7 @@ CREATE TABLE `magazine` (
 
 LOCK TABLES `magazine` WRITE;
 /*!40000 ALTER TABLE `magazine` DISABLE KEYS */;
-INSERT INTO `magazine` VALUES (1,1,'2016-10-14',1,0,'2016-10-14',1);
+INSERT INTO `magazine` VALUES (1,13,'2016-10-12',1,0,'2016-10-15',2);
 /*!40000 ALTER TABLE `magazine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -415,10 +410,13 @@ CREATE TABLE `reservation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
   `date_expiration` date DEFAULT NULL,
-  `usager_id` bigint(20) DEFAULT NULL,
+  `usager_reservation_id` bigint(20) DEFAULT NULL,
+  `exemplaire_reservation_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_reservation_usager_id` (`usager_id`),
-  CONSTRAINT `fk_reservation_usager_id` FOREIGN KEY (`usager_id`) REFERENCES `usager` (`id`)
+  KEY `fk_reservation_usager_reservation_id` (`usager_reservation_id`),
+  KEY `fk_reservation_exemplaire_reservation_id` (`exemplaire_reservation_id`),
+  CONSTRAINT `fk_reservation_exemplaire_reservation_id` FOREIGN KEY (`exemplaire_reservation_id`) REFERENCES `exemplaire` (`id`),
+  CONSTRAINT `fk_reservation_usager_reservation_id` FOREIGN KEY (`usager_reservation_id`) REFERENCES `usager` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -428,7 +426,7 @@ CREATE TABLE `reservation` (
 
 LOCK TABLES `reservation` WRITE;
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` VALUES (1,'2016-10-13','2016-10-20',1);
+INSERT INTO `reservation` VALUES (1,'2016-10-15','2016-10-22',1,1);
 /*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -454,7 +452,7 @@ CREATE TABLE `usager` (
 
 LOCK TABLES `usager` WRITE;
 /*!40000 ALTER TABLE `usager` DISABLE KEYS */;
-INSERT INTO `usager` VALUES (1,'Chirac','Jacques','13 rue Michel Ney, 54000 Nancy');
+INSERT INTO `usager` VALUES (1,'CHIRAC','Jacques','13 rue Michel Ney, 54000 Nancy');
 /*!40000 ALTER TABLE `usager` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -467,4 +465,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-14 17:51:21
+-- Dump completed on 2016-10-15 15:54:07
