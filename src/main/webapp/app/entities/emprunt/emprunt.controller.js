ca5a785f240a8dea9bf5ec3fa4c1401f5e48f9e8
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2016-11-03 20:01:14 +0100

    Filtre (En retard|Dans les temps)

--- src/main/webapp/app/entities/emprunt/emprunt.controller.js
+++ src/main/webapp/app/entities/emprunt/emprunt.controller.js
@@ -9,8 +9,25 @@
 
     function EmpruntController ($scope, $state, Emprunt) {
         var vm = this;
-        
+
         vm.emprunts = [];
+
+
+        var enRetard = function (item) {
+            return new Date(item.dateRetour) < Date.now();
+        };
+
+        var nonEnRetard = function (item) {
+            return !enRetard(item);
+        };
+
+        var someEnRetard = function(){
+            return vm.emprunts.some(enRetard);
+        };
+
+        vm.enRetard = enRetard;
+        vm.nonEnRetard = nonEnRetard;
+        vm.someEnRetard = someEnRetard;
 
         loadAll();
 
