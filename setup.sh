3a8944382df27d16442ac4f973d97344eb53fad2
Guillaume Denis <denisgme@gmail.com>
2016-11-06 20:33:22 +0100

    add: setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm install
+gulp install
+bower install
+
+./mvnw clean package -Dmaven.test.skip=true
+
+cp target/bibal-denis-0.0.1-SNAPSHOT.war src/main/docker/app.war
+cd src/main/docker
+
+docker-compose up
